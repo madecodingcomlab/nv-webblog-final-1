0b512b1499d510f3f73bd9c0467dfea7cda572d3
PikachuCMD <77847699+PikachuCMD@users.noreply.github.com>
2022-10-09 16:39:22 +0700

    first commit

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,15 +1,15 @@
-module.exports = {   
-    port: 8081,     
+module.exports = {
+    port: 8081,
     db: {
         database: process.env.DB_NAME || 'Com-gt',
         user: process.env.DB_User || 'root',
         password: process.env.DB_PASS || '',
         options: {
-        dialect: process.env.DIALECT || 'sqlite',//sqlite mysql
-        storage: './Com-gt.sqlite'
-        //host: process.env.HOST || 'localhost',
+            dialect: process.env.DIALECT || 'sqlite', //sqlite mysql
+            storage: './Kpop_Mart.sqlite'
+                //host: process.env.HOST || 'localhost',
         },
-        },  
+    },
     authentication: {
         jwtSecret: 'test'
     }
